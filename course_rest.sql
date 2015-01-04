2a033ab4f9f63ae5c0621e29d58875f36aa23ea9
Enrique M. Talavera <kiquetal@kathy.site>
2015-01-04 16:59:39 -0300

    Rest via php slim

---
+++ course_rest.sql
@@ -0,0 +1,131 @@
+-- MySQL dump 10.13  Distrib 5.1.69, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: course_rest
+-- ------------------------------------------------------
+-- Server version	5.1.69-0ubuntu0.11.10.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `course_rest`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `course_rest` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `course_rest`;
+
+--
+-- Table structure for table `skills`
+--
+
+DROP TABLE IF EXISTS `skills`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `skills` (
+  `id` int(11) NOT NULL,
+  `name` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `skills`
+--
+
+LOCK TABLES `skills` WRITE;
+/*!40000 ALTER TABLE `skills` DISABLE KEYS */;
+INSERT INTO `skills` VALUES (100,'PHP'),(200,'DATABASE'),(300,'JAVASCRIPT'),(400,'SYS-ADMINISTRATION');
+/*!40000 ALTER TABLE `skills` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'kiquetal'),(2,'kiquetal'),(3,'kiquetal');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'NERY'),(2,'RODRIGO'),(3,'PABLO'),(4,'JOSEF'),(5,'RICARDO');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users_skills`
+--
+
+DROP TABLE IF EXISTS `users_skills`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users_skills` (
+  `id_user` int(11) NOT NULL DEFAULT '0',
+  `id_skill` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_skill`,`id_user`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users_skills`
+--
+
+LOCK TABLES `users_skills` WRITE;
+/*!40000 ALTER TABLE `users_skills` DISABLE KEYS */;
+INSERT INTO `users_skills` VALUES (1,100),(2,100),(3,300),(5,300),(4,400);
+/*!40000 ALTER TABLE `users_skills` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-01-01 13:33:15
---
+++ index.php
@@ -0,0 +1,440 @@
+<?php
+require 'Slim/Slim.php';
+\Slim\Slim::registerAutoloader();
+require 'rb.php';
+
+
+R::setup('mysql:host=localhost;
+        dbname=course_rest','kiquetal','paraguay');
+
+
+$app=new \Slim\Slim();
+
+
+
+
+
+
+$valores=array();
+
+for ($i=0;$i<10;$i++)
+{
+ $valores[]=array('id'=>rand()*10+1,'name'=>'algo');
+}
+$app->get('/items',function() use ($app,$valores)
+ {
+
+ $app->response->headers->set('Content-Type','application/json');
+ $app->response->setBody(json_encode($valores));
+ $app->response->setStatus(200);
+
+});
+
+$app->post('/items',function()use ($app)
+{
+ $body=$app->request->getBody(); 
+ $object_body=json_decode($body,true);
+ $object_body["aatr"]="nuevo";
+ $app->response->headers->set('Content-Type','application/json');
+ $app->response->setBody(json_encode($object_body));
+
+
+});
+
+$app->get('/users',function() use ($app)
+{
+   $users=R::getAll( 'select * from users' );
+   $app->response->headers->set('Content-Type','application/json');
+   $app->response->setStatus(200);
+  echo json_encode($users,true);
+
+
+});
+
+
+
+
+
+$app->post('/users',function() use ($app)
+{
+
+ $header=$app->request->headers->get('Content-Type');
+ if ($header!='application/json')
+{
+   $app->response->setStatus(400);
+   $app->response->headers->set('Content-Type','application/json');
+   $app->response->setBody(json_encode(array('status'=>"ERROR",'response'=>'CONTENT-TYPE MUST BE APPLICATION/JSON')));
+    
+}
+ else
+{
+
+  $body=$app->request->getBody();
+  $object=json_decode($body,true);
+  $user=R::dispense('users');
+ try
+{
+  $user->name=$object['name'];
+  if ($user->name==null) throw new Exception("Debe enviar el campo nombre");
+  $id=R::store($user);
+  $app->response->setStatus(201);
+  $app->response->headers->set('Content-Type','application/json');
+  $app->response->setBody(json_encode(array('status'=>'OK','response'=>array('message'=>'New user created','code'=>$id))));
+ }
+catch(Exception $e)
+{
+$app->response->setStatus(400);
+$app->response->headers->set('Content-Type','application/json');
+$app->response->setBody(json_encode(array('status'=>'ERROR','response'=>array('message'=>$e->getMessage()))));
+}  
+
+
+}
+
+});
+
+
+$app->get("/users/:id/skills",function($id) use ($app)
+{
+
+try
+{
+ $user=R::findOne('users','id=:id',array('id'=>$id));
+ if ($user!=null)
+  {
+
+  $app->response->setStatus(200);
+  $app->response->headers->set('Content-Type','application/json');
+//  $app->response->setStatus(200);
+//  $app->response->setBody(json_encode(array('status'=>'OK','response'=>json_decode($user,true))));
+$skills=R::getAll( 'SELECT us.id_skill,s.name FROM users_skills us JOIN skills s ON (s.id=us.id_skill)
+    WHERE us.id_user = :user_id',
+       array(':user_id' => $id));
+  $app->response->setBody(json_encode(array('status'=>'OK','response'=>array('skills'=>$skills)))); 
+
+
+ // if 
+
+
+
+  
+}
+else
+{
+ 
+$app->response->headers->set('Content-Type','application/json');
+$app->response->setStatus(404);
+$app->response->setBody(json_encode(array('status'=>'ERROR','response'=>'User WAS NOT FOUND')));
+
+
+}
+}
+catch(Exception $e)
+{
+
+
+$app->response->headers->set('Content-Type','application/json');
+$app->response->setStatus(500);
+$app->response->setBody(json_encode(array('status'=>'ERROR','response'=>$e->getMessage())));
+
+
+
+}
+});
+
+
+$app->delete('/users/:id/skills/:id_skill',function($id,$skill_id) use ($app)
+{
+
+   $user=R::findOne('users','id=:id',array('id'=>$id));
+ // echo $user;
+// return; 
+if ($user!=null)
+{
+
+  $skill=R::findOne('skills','id=:id',array('id'=>$skill_id));
+ 
+ if($skill!=null)
+{
+   
+  $skillUser=R::getAll( 'SELECT * FROM users_skills where id_user=:id_user and id_skill=:id_skill',
+       array('id_user'=>$id,'id_skill'=>$skill_id));
+   if ($skillUser!=null)
+{
+
+try
+{
+
+    R::exec( 'DELETE FROM  users_skills where id_user=:id_user and id_skill=:id_skill',array('id_user'=>$id,'id_skill'=>$skill_id));
+
+$app->response->headers->set('Content-type','application/json');
+  $app->response->setStatus(200);
+  $app->response->setBody(json_encode(array('status'=>'OK','response'=>'USER HAS NOT THE SKILL ANYMORE')));
+
+}
+catch(Exception $e)
+{
+
+$app->response->headers->set('Content-type','application/json');
+  $app->response->setStatus(500);
+  $app->response->setBody(json_encode(array('status'=>'ERROR','response'=>$e->getMessage())));
+
+
+}
+
+
+}   
+  else
+  {
+$app->response->headers->set('Content-type','application/json');
+  $app->response->setStatus(400);
+  $app->response->setBody(json_encode(array('status'=>'ERROR','response'=>'USER HAS NOT THE DESIRED SKILLS')));
+}
+
+    
+
+
+
+}
+else
+{
+$app->response->headers->set('Content-type','application/json');
+  $app->response->setStatus(400);
+  $app->response->setBody(json_encode(array('status'=>'ERROR','response'=>'SKILL DOES NOT EXISTS')));
+
+
+}
+
+
+}
+else
+{
+
+
+  $app->response->headers->set('Content-type','application/json');
+  $app->response->setStatus(400);
+  $app->response->setBody(json_encode(array('status'=>'ERROR','response'=>'USER NOT FOUND')));
+
+
+
+}
+
+});
+
+$app->post('/users/:id/skills',function($id) use ($app)
+{
+ 
+ $user=R::findOne('users','id=:id',array('id'=>$id));
+ // echo $user;
+// return; 
+if ($user!=null)
+  {
+
+ // $app->response->setStatus(200);
+ // $app->response->headers->set('Content-Type','application/json');
+//  $app->response->setStatus(200);
+//  $app->response->setBody(json_encode(array('status'=>'OK','response'=>json_decode($user,true))));
+
+
+$header=$app->request->headers->get('Content-Type');
+ if ($header!='application/json')
+{
+   $app->response->setStatus(400);
+   $app->response->headers->set('Content-Type','application/json');
+   $app->response->setBody(json_encode(array('status'=>"ERROR",'response'=>'CONTENT-TYPE MUST BE APPLICATION/JSON')));
+
+}
+ else
+{
+
+try
+{
+  $body=$app->request->getBody();
+  $object=json_decode($body,true);
+  $skills=R::dispense('skills');
+  $skill->name=$object['name'];
+  $rowSkill=R::getRow("Select * from skills where name LIKE ?",array('%'.$object['name'].'%'));
+  if($rowSkill!=null)
+{
+
+ $rowMax=$rowSkill['id'];
+
+}
+else
+{
+ $rowMax=R::getCell('Select max(id) from skills');
+
+
+
+ $skillNew=R::dispense('skills');
+ $skillNew->id=$rowMax+1;
+ 
+$skillNew->name=$object['name'];
+
+ $id_skill_new=R::store($skillNew);
+ $rowMax=$rowMax+1;
+}
+//return; 
+
+
+    R::exec( 'INSERT INTO users_skills(id_user,id_skill) VALUES('.$id.','.$rowMax.')');
+  
+$app->response->setStatus(201);
+   $app->response->headers->set('Content-Type','application/json');
+   $app->response->setBody(json_encode(array('status'=>"OK",'response'=>'New skill were added')));
+
+
+ }
+catch(Exception $e)
+{
+
+  $app->response->setStatus(400);
+   $app->response->headers->set('Content-Type','application/json');
+   $app->response->setBody(json_encode(array('status'=>"ERROR",'response'=>$e->getMessage())));
+}
+}
+}
+
+
+else
+{
+
+$app->response->headers->set('Content-Type','application/json');
+$app->response->setStatus(404);
+$app->response->setBody(json_encode(array('status'=>'ERROR','response'=>'User WAS NOT FOUND')));
+
+
+
+
+}
+
+
+
+});
+
+
+$app->get("/users/:id", function($id) use ($app)
+{
+
+try
+{
+
+ $user=R::findOne('users','id=:id',array('id'=>$id));
+ if ($user!=null)
+  {
+
+  $app->response->headers->set('Content-Type','application/json');
+  $app->response->setStatus(200);
+  $app->response->setBody(json_encode(array('status'=>'OK','response'=>json_decode($user,true))));
+//echo $usero
+
+}
+else
+{
+
+ $app->response->headers->set('Content-Type','application/json');
+ $app->response->setStatus(404);
+ $app->response->setBody(json_encode(array('status'=>'ERROR','response'=>'User not found')));
+
+
+
+}
+
+}
+catch(Exception $e)
+{
+ $app->response->headers->set('Content-Type','application/json');
+ $app->response->setStatus(500);
+ $app->response->setBody(json_encode(array('status'=>'ERROR','repsonse'=>$e->getMessage())));
+
+
+}
+
+});
+
+
+$app->delete("/users/:id",function($id) use ($app)
+{
+
+ $user=R::findOne('users','id=:id',array('id'=>$id));
+
+  if($user!=null){
+    // echo json_encode($user);
+
+try
+{
+ R::trash($user);
+ $app->response->headers->set('Content-Type','application/json');
+ $app->response->setStatus(200);
+ $app->response->setBody(json_encode(array('status'=>'OK','response'=>'The user was deleted')));
+}
+catch(Exception $e)
+{
+
+ $app->response->headers->set('Content-Type','application/json');
+ $app->response->setStatus(500);
+ $app->response->setBody(json_encode(array('status'=>'ERROR','response'=>$e->getMessage())));
+
+
+}
+
+
+
+
+}
+  else
+{
+  
+  $app->response->headers->set('Content-type','application/json');
+  $app->response->setStatus(400);
+  $app->response->setBody(json_encode(array('status'=>'ERROR','response'=>'USER NOT FOUND')));
+}
+
+});
+
+
+$app->get('/items/all',function() use ($app,$valores)
+{
+
+  $accept=$app->request->headers->get('Accept');
+  if ($accept=='text/xml')
+{
+$app->response->headers->set('Content-Type','text/xml');
+$x=new SimpleXMLElement("<root/>");
+$items=$x->addChild("items");
+ foreach($valores as $k=>$v)
+{
+
+ $item=$items->addChild('item'); 
+ $item->addChild('id',$v['id']);
+ $item->addChild('name',$v['name']);
+
+}
+echo $x->asXml();
+
+}
+else
+    {
+    $app->response->headers->set('Content-Type','application/json');
+//    echo json_encode(array('root'=>$valores));
+$items=array(); 
+ foreach($valores as $k=>$v)
+{
+$items[]=array('id'=>$v['id'],'name'=>$v['name']);
+}
+echo json_encode(array('root'=>array('items'=>$items)));
+
+}
+
+});
+
+
+$app->notFound(function () use ($app) {
+//    $app->render('error.php');
+  echo 'NOT IMPLEMENTED YET. ';
+});
+
+$app->run();
+
+
+?>
